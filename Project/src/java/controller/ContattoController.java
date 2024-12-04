0d827782e44d5b528caacd3d636fd3dedab8534d
Pietroanzalone <p.anzalone@studenti.unisa.it>
2024-12-04 11:57:49 +0100

    Inizializzazione del package Controller
    Popolati i file del package controller con e le classi e ei metodi iniziali, attualmente non implementati (throw UnsupportedOperationException).

---
+++ Project/src/java/controller/ContattoController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import model.*;
+
+public class ContattoController {
+
+    public void visualizza(Contatto c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void modifica(Contatto nuovo) {
+        throw new UnsupportedOperationException("Not supported yet.")
+    }
+
+    public void elimina(Contatto c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void apriEmail(String email) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void indietro() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ Project/src/java/controller/ModificaContattoController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.*;
+
+public class ModificaContattoController {
+
+    public void aggiungi(Contatto c) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void modifica(Contatto vecchio,Contatto nuovo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void annulla(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
