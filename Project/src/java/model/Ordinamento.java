974de979174956a20bff94c7c7fd1233d1ab939f
salvaddeo <s.addeo5@studenti.unisa.it>
2024-12-05 16:53:12 +0100

    Modifca classi package model
    - Modificati i messaggi di eccezioni delle classi del package "model" per una migliore chiarezza

--- Project/src/java/model/Ordinamento.java
+++ Project/src/java/model/Ordinamento.java
@@ -6,24 +6,24 @@ public class Ordinamento implements Comparator<Contatto> {
     private boolean ordinamentoPerNome;
 
     public Ordinamento() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Ordinamento constructor is not supported yet.");
     }
 
     public boolean isOrdinamentoPerNome() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Ordinamento.isOrdinamentoPerNome is not supported yet.");
     }
 
     public void setOrdinamento(boolean ordinamentoPerNome) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Ordinamento.setOrdinamento is not supported yet.");
     }
 
     public void cambiaOrdinamento() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Ordinamento.cambiaOrdinamento is not supported yet.");
     }
 
     @Override
     public int compare(Contatto o1, Contatto o2) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Ordinamento.compare is not supported yet.");
     }
 
 }
--- Project/src/java/model/Rubrica.java
+++ Project/src/java/model/Rubrica.java
@@ -6,35 +6,35 @@ public class Rubrica {
     private Set<Contatto> contatti;
 
     public Rubrica(Ordinamento o) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica constructor (Ordinamento o) is not supported yet.");
     }
 
     public Rubrica(Set<Contatto> contatti, Ordinamento o) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica constructor (Set<Contatto> contatti, Ordinamento o) is not supported yet.");
     }
 
     public Set<Contatto> getContatti() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica.getContatti is not supported yet.");
     }
 
     public void aggiungi(Contatto c) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica.aggiungi is not supported yet.");
     }
 
     public void rimuovi(Contatto c) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica.rimuovi is not supported yet.");
     }
 
     public void modifica(Contatto vecchio, Contatto nuovo) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica.modifica is not supported yet.");
     }
 
     public String esporta() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica.esporta is not supported yet.");
     }
 
     public Set<Contatto> cerca(String stringa) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Rubrica.cerca is not supported yet.");
     }
 
 }
